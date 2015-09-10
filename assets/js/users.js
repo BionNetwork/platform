122dfc3b48b9dd2e3dd7647237f72eea02533b66
CaLYpSE <mrcalypse13@mail.ru>
2015-09-10 15:18:08 +0300

    refs #2894 pagination

--- assets/js/users.js
+++ assets/js/users.js
@@ -16,3 +16,46 @@ function removeUser(url){
         cancelButton: "Отмена"
     });
 }
+
+
+var activePage, usersUrl;
+
+$(document).ready(function(){
+    activePage = $('#active_num').val();
+    usersUrl = $('#users_table').attr('data-url');
+
+    $('[name="birth_date"]').datepicker({
+        format: 'dd.mm.yyyy',
+        autoclose: true,
+        forseParse: false,
+        keyboardNavigation: false
+    });
+
+   $('[name="phone"]').inputmask("99999999999");
+
+});
+
+function prev(){
+    var search = $('#search').val();
+    activePage = activePage==0 ? activePage : activePage - 1
+    document.location = usersUrl+'?page='+activePage+';search='+search;
+}
+
+function next(){
+    var search = $('#search').val(),
+        current = $("li.active[data-pagi='true']");
+    if(!current.attr('data-max')){
+        activePage = activePage + 1
+    }
+    document.location = usersUrl+'?page='+activePage+';search='+search;
+}
+
+function pagi(ind){
+    var search = $('#search').val();
+    document.location = usersUrl+'?page='+ind+';search='+search;
+}
+
+function search(){
+    var search = $('#search').val();
+    document.location = usersUrl+'?search='+search;
+}
