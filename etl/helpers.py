d1cd11e05cba38163f6b7f993f021e34040731ec
miholeus <me@miholeus.com>
2015-09-14 19:48:52 +0300

    utf-8 пока не работает

--- etl/helpers.py
+++ etl/helpers.py
@@ -7,21 +7,35 @@ import MySQLdb
 from core.models import ConnectionChoices
 
 
-def check_connection(post):
+def get_utf8_string(value):
+    """
+    Кодирование в utf-8 строки
+    :param value: string
+    :return: string
+    """
+    return unicode(value)
+
+
+def check_connection(data):
+    """
+    Проверка соединения источников данных
+    :param data: dict
+    :return: bool
+    """
     conn_info = {
-        'host': post.get('host', ''),
-        'user': post.get('login', ''),
-        'passwd': post.get('password', ''),
-        'db': post.get('name', ''),
-        'port': int(post.get('port', ''))
+        'host': get_utf8_string(data.get('host', '')),
+        'user': get_utf8_string(data.get('login', '')),
+        'passwd': get_utf8_string(data.get('password', '')),
+        'db': get_utf8_string(data.get('name', '')),
+        'port': int(data.get('port', ''))
     }
 
-    conn_type = int(post.get('conn_type', ''))
+    conn_type = int(data.get('conn_type', ''))
 
     if conn_type == ConnectionChoices.POSTGRESQL:
         try:
-            conn_str = ("host='{host}' dbname='{db}' user='{user}' "
-                        "password='{passwd}' port={port}").format(**conn_info)
+            conn_str = (u"host='{host}' dbname='{db}' user='{user}' "
+                        u"password='{passwd}' port={port}").format(**conn_info)
             psycopg2.connect(conn_str)
         except psycopg2.OperationalError:
             return False
@@ -30,5 +44,7 @@ def check_connection(post):
             MySQLdb.connect(**conn_info)
         except MySQLdb.OperationalError:
             return False
+    else:
+        raise ValueError("Неизвестный тип подключения")
 
     return True
