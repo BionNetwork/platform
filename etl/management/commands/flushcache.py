90a308644694b2e03dae0140853a2c57ac120229
miholeus <me@miholeus.com>
2015-09-17 17:04:15 +0300

    fixes #2984 удаление кеша

---
+++ etl/management/commands/flushcache.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+__author__ = 'miholeus'
+
+from django.core.management.base import BaseCommand
+from django.conf import settings
+
+import redis
+
+
+class Command(BaseCommand):
+    args = '<>'
+    help = "Сбрасывание кеша"
+
+    def handle(self, *args, **options):
+        r_server = redis.StrictRedis(
+            host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB)
+        r_server.flushdb()
+        print 'Cleaned cache!'
