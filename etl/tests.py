b985a97adac613ea96f84609d9764e5f4cfc13b3
CaLYpSE <mrcalypse13@mail.ru>
2015-10-22 14:08:39 +0300

    refs #3238 test fix

--- etl/tests.py
+++ etl/tests.py
@@ -11,90 +11,90 @@ from etl.helpers import Postgresql, TablesTree
 Проверка метаданных
 """
 
-#
-# class DatabaseTest(TestCase):
-#
-#     def setUp(self):
-#         connection = {'host': 'localhost', 'port': 5432, 'db': 'test', 'user': 'foo', 'passwd': 'bar'}
-#         # connection = {'host': 'localhost', 'port': 5432, 'db': 'biplatform',
-#         #               'user': 'biplatform', 'passwd': 'biplatform'}
-#         self.database = Postgresql(connection)
-#         self.maxDiff = None
-#
-#     def test_check_connection(self):
-#         self.assertNotEqual(self.database.connection, None, u"Подключение к СУБД не удалось!")
-#
-#     def test_generate_join(self):
-#         structure = {'childs': [{'childs': [], 'joins': [
-#             {'right': {'column': 'billing_bank_packet_status_id', 'table': 'billing_bank_packet'},
-#              'join': {'type': 'inner', 'value': 'eq'},
-#              'left': {'column': 'id', 'table': 'billing_bank_packet_status'}}], 'join_type': 'inner',
-#                                  'val': 'billing_bank_packet'}], 'joins': [], 'join_type': 'inner',
-#                      'val': 'billing_bank_packet_status'}
-#         join_query = self.database.generate_join(structure)
-#         expected_join_query = ("billing_bank_packet_status INNER JOIN billing_bank_packet",
-#                                "ON billing_bank_packet_status.id = billing_bank_packet.billing_bank_packet_status_id")
-#         self.assertEqual(' '.join(expected_join_query), join_query)
-#
-#     def test_generate_join_multiple_tables(self):
-#         structure = {
-#             "childs": [
-#                 {
-#                     "childs": [
-#                         {
-#                             "childs": [
-#                             ],
-#                             "join_type": "inner",
-#                             "joins": [
-#                                 {
-#                                     "join": {
-#                                         "type": "inner",
-#                                         "value": "eq"
-#                                     },
-#                                     "left": {
-#                                         "column": "billing_bank_packet_id",
-#                                         "table": "billing_bank_packet_operation"
-#                                     },
-#                                     "right": {
-#                                         "column": "id",
-#                                         "table": "billing_bank_packet"
-#                                     }
-#                                 }
-#                             ],
-#                             "val": "billing_bank_packet_operation"
-#                         }
-#                     ],
-#                     "join_type": "inner",
-#                     "joins": [
-#                         {
-#                             "join": {
-#                                 "type": "inner",
-#                                 "value": "eq"
-#                             },
-#                             "left": {
-#                                 "column": "id",
-#                                 "table": "billing_bank_packet_status"
-#                             },
-#                             "right": {
-#                                 "column": "billing_bank_packet_status_id",
-#                                 "table": "billing_bank_packet"
-#                             }
-#                         }
-#                     ],
-#                     "val": "billing_bank_packet"
-#                 }
-#             ],
-#             "join_type": "inner",
-#             "joins": [
-#             ],
-#             "val": "billing_bank_packet_status"
-#         }
-#         join_query = self.database.generate_join(structure)
-#         expected_join_query = ("billing_bank_packet_status INNER JOIN billing_bank_packet",
-#                                "ON billing_bank_packet_status.id = billing_bank_packet.billing_bank_packet_status_id",
-#                                "INNER JOIN billing_bank_packet_operation ON",
-#                                "billing_bank_packet_operation.billing_bank_packet_id = billing_bank_packet.id")
-#         self.assertEqual(' '.join(expected_join_query), join_query)
+
+class DatabaseTest(TestCase):
+
+    def setUp(self):
+        connection = {'host': 'localhost', 'port': 5432, 'db': 'test', 'user': 'foo', 'passwd': 'bar'}
+        # connection = {'host': 'localhost', 'port': 5432, 'db': 'biplatform',
+        #               'user': 'biplatform', 'passwd': 'biplatform'}
+        self.database = Postgresql(connection)
+        self.maxDiff = None
+
+    def test_check_connection(self):
+        self.assertNotEqual(self.database.connection, None, u"Подключение к СУБД не удалось!")
+
+    def test_generate_join(self):
+        structure = {'childs': [{'childs': [], 'joins': [
+            {'right': {'column': 'billing_bank_packet_status_id', 'table': 'billing_bank_packet'},
+             'join': {'type': 'inner', 'value': 'eq'},
+             'left': {'column': 'id', 'table': 'billing_bank_packet_status'}}], 'join_type': 'inner',
+                                 'val': 'billing_bank_packet'}], 'joins': [], 'join_type': 'inner',
+                     'val': 'billing_bank_packet_status'}
+        join_query = self.database.generate_join(structure)
+        expected_join_query = ("billing_bank_packet_status INNER JOIN billing_bank_packet",
+                               "ON billing_bank_packet_status.id = billing_bank_packet.billing_bank_packet_status_id")
+        self.assertEqual(' '.join(expected_join_query), join_query)
+
+    def test_generate_join_multiple_tables(self):
+        structure = {
+            "childs": [
+                {
+                    "childs": [
+                        {
+                            "childs": [
+                            ],
+                            "join_type": "inner",
+                            "joins": [
+                                {
+                                    "join": {
+                                        "type": "inner",
+                                        "value": "eq"
+                                    },
+                                    "left": {
+                                        "column": "billing_bank_packet_id",
+                                        "table": "billing_bank_packet_operation"
+                                    },
+                                    "right": {
+                                        "column": "id",
+                                        "table": "billing_bank_packet"
+                                    }
+                                }
+                            ],
+                            "val": "billing_bank_packet_operation"
+                        }
+                    ],
+                    "join_type": "inner",
+                    "joins": [
+                        {
+                            "join": {
+                                "type": "inner",
+                                "value": "eq"
+                            },
+                            "left": {
+                                "column": "id",
+                                "table": "billing_bank_packet_status"
+                            },
+                            "right": {
+                                "column": "billing_bank_packet_status_id",
+                                "table": "billing_bank_packet"
+                            }
+                        }
+                    ],
+                    "val": "billing_bank_packet"
+                }
+            ],
+            "join_type": "inner",
+            "joins": [
+            ],
+            "val": "billing_bank_packet_status"
+        }
+        join_query = self.database.generate_join(structure)
+        expected_join_query = ("billing_bank_packet_status INNER JOIN billing_bank_packet",
+                               "ON billing_bank_packet_status.id = billing_bank_packet.billing_bank_packet_status_id",
+                               "INNER JOIN billing_bank_packet_operation ON",
+                               "billing_bank_packet_operation.billing_bank_packet_id = billing_bank_packet.id")
+        self.assertEqual(' '.join(expected_join_query), join_query)
 
 
 class TablesTreeTest(TestCase):
