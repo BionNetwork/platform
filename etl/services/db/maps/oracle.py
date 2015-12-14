6a27e3ee4989bd1bf266bdee77eb0787d2175b39
ifattakhov <comm.juve@gmail.com>
2015-12-14 18:19:02 +0300

    refs #4156 Подключение Oracle в качестве источника

--- etl/services/db/maps/oracle.py
+++ etl/services/db/maps/oracle.py
@@ -46,3 +46,22 @@ for i in texts:
 
 for i in dates:
     ORACLE_TYPES[i] = 'timestamp'
+
+
+constraints_query = """
+    SELECT ucc1.table_name, ucc1.column_name, uc.constraint_name, uc.constraint_type,
+    ucc2.table_name, ucc2.column_name, '', uc.delete_rule
+    FROM user_constraints uc, user_cons_columns ucc1, user_cons_columns ucc2
+    WHERE ucc1.table_name in {0}
+    AND uc.constraint_name = ucc1.constraint_name
+    AND uc.r_constraint_name = ucc2.constraint_name
+    AND ucc1.POSITION = ucc2.POSITION
+    ORDER BY ucc1.TABLE_NAME, uc.constraint_name
+    """
+
+
+indexes_query = """
+    SELECT i.table_name, c.column_name, i.index_name, i.uniqueness
+    FROM all_ind_columns c, all_indexes i
+    WHERE i.table_name IN {0} and i.index_name = c.index_name
+"""
--- etl/services/db/oracle.py
+++ etl/services/db/oracle.py
@@ -61,13 +61,11 @@ class Oracle(Database):
         cols_query = """select table_name, column_name, data_type from user_tab_columns
           where table_name in {0}""".format(tables_str)
 
-        # cols_query = oracle_map.cols_query.format(tables_str, source.db, 'public')
-
-        # constraints_query = oracle_map.constraints_query.format(tables_str)
+        constraints_query = oracle_map.constraints_query.format(tables_str)
         #
-        # indexes_query = oracle_map.indexes_query.format(tables_str)
+        indexes_query = oracle_map.indexes_query.format(tables_str)
 
-        return cols_query, '', ''
+        return cols_query, constraints_query, indexes_query
 
 
     def get_columns(self, source, tables):
@@ -81,8 +79,14 @@ class Oracle(Database):
             source, tables)
 
         col_records = self.get_query_result(columns_query)
-        index_records = ()
-        const_records = ()
+        index_records = []
+        for record in self.get_query_result(indexes_query):
+            index_records.append(
+                (record[0], record[1], record[2],
+                 't' if record[2].endswith('PK') else 'f',
+                 't' if record[3] == 'UNIQUE' else 'f'))
+
+        const_records = self.get_query_result(consts_query)
 
         return col_records, index_records, const_records
 
@@ -106,7 +110,6 @@ class Oracle(Database):
         # TODO: Переписать
         return {x: None for x in tables}
 
-
     @classmethod
     def processing_records(cls, col_records, index_records, const_records):
         """
@@ -116,45 +119,83 @@ class Oracle(Database):
         :param const_records: str
         :return: tuple
         """
-
+        # fixme: повторяет код
         indexes = defaultdict(list)
+        itable_name, icol_names, index_name, primary, unique = xrange(5)
+
+        for ikey, igroup in groupby(index_records, lambda x: x[itable_name]):
+            for ig in igroup:
+                indexes[ikey].append({
+                    "name": ig[index_name],
+                    "columns": ig[icol_names].split(','),
+                    "is_primary": ig[primary] == 't',
+                    "is_unique": ig[unique] == 't',
+                })
+
+        constraints = defaultdict(list)
+        (c_table_name, c_col_name, c_name, c_type,
+         c_foreign_table, c_foreign_col, c_update, c_delete) = xrange(8)
+
+        for ikey, igroup in groupby(const_records, lambda x: x[c_table_name]):
+            for ig in igroup:
+                constraints[ikey].append({
+                    "c_col_name": ig[c_col_name],
+                    "c_name": ig[c_name],
+                    "c_type": ig[c_type],
+                    "c_f_table": ig[c_foreign_table],
+                    "c_f_col": ig[c_foreign_col],
+                    "c_upd": ig[c_update],
+                    "c_del": ig[c_delete],
+                })
+
         columns = defaultdict(list)
         foreigns = defaultdict(list)
 
         table_name, col_name, col_type = xrange(3)
 
         for key, group in groupby(col_records, lambda x: x[table_name]):
-            #
-            # t_indexes = indexes[key]
-            # t_consts = constraints[key]
+
+            t_indexes = indexes[key]
+            t_consts = constraints[key]
 
             for x in group:
                 is_index = is_unique = is_primary = False
                 col = x[col_name]
-                #
-                # for i in t_indexes:
-                #     if col in i['columns']:
-                #         is_index = True
-                #         index_name = i['name']
-                #         for c in t_consts:
-                #             const_type = c['c_type']
-                #             if index_name == c['c_name']:
-                #                 if const_type == 'UNIQUE':
-                #                     is_unique = True
-                #                 elif const_type == 'PRIMARY KEY':
-                #                     is_unique = True
-                #                     is_primary = True
+
+                for i in t_indexes:
+                    if col in i['columns']:
+                        is_index = True
+                        index_name = i['name']
+                        for c in t_consts:
+                            const_type = c['c_type']
+                            if index_name == c['c_name']:
+                                if const_type == 'U':
+                                    is_unique = True
+                                elif const_type == 'P':
+                                    is_unique = True
+                                    is_primary = True
 
                 columns[key].append({"name": col,
-                                     "type": (
-                                         oracle_map.ORACLE_TYPES[
-                                            cls.lose_brackets(x[col_type])]
-                                            or x[col_type]),
+                                     "type": (oracle_map.ORACLE_TYPES[
+                                                  cls.lose_brackets(x[col_type])]
+                                              or x[col_type]),
                                      "is_index": is_index,
                                      "is_unique": is_unique,
                                      "is_primary": is_primary})
 
+            # находим внешние ключи
+            for c in t_consts:
+                if c['c_type'] == 'R':
+                    foreigns[key].append({
+                        "name": c['c_name'],
+                        "source": {"table": key, "column": c["c_col_name"]},
+                        "destination":
+                            {"table": c["c_f_table"], "column": c["c_f_col"]},
+                        "on_delete": c["c_del"],
+                        "on_update": c["c_upd"],
+                    })
         return columns, indexes, foreigns
+
 
 
     @staticmethod
