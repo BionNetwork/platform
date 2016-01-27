9afb5eb7c95647bb5e1410d40f2617d311b3f2d5
Andres Rios <rianby64@gmail.com>
2016-01-27 17:56:52 +0300

    Add gulp with default task

---
+++ assets/angular/gulpfile.js
@@ -0,0 +1,38 @@
+
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    //concat = require('gulp-concat'),
+    minifyJS = require('gulp-uglify'),
+    minifyCSS = require('gulp-minify-css'),
+    usemin = require('gulp-usemin');
+
+
+var paths = {
+  dist: 'dist/',
+  index: 'src/index.html'
+};
+
+gulp.task('usemin', function() {
+  return gulp.src(paths.index)
+    .pipe(
+      usemin(
+        {
+          js: [
+            minifyJS(),
+            'concat'
+          ],
+          css: [
+            minifyCSS({
+              keepSpecialComments: 0
+            }),
+            'concat'
+          ],
+        }
+      )
+    )
+    .pipe(gulp.dest('dist/'));
+});
+
+
+gulp.task('default', ['usemin']);
+
