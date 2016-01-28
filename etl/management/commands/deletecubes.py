ca518b8afabcf731553d4d6eea9738417ec503be
ifattakhov <comm.juve@gmail.com>
2016-01-28 14:20:56 +0300

    refs #4470 @1.5h корректное удаление datasettometa

--- etl/management/commands/deletecubes.py
+++ etl/management/commands/deletecubes.py
@@ -40,13 +40,13 @@ class Command(BaseCommand):
         Dimension.objects.filter(datasources_meta_id__in=meta_ids).delete()
         Measure.objects.filter(datasources_meta_id__in=meta_ids).delete()
 
-        dtms = DatasetToMeta.objects.filter(dataset__key__in=meta_values)
+        cubes_name = ['cube_%s' % x for x in meta_values]
+        Cube.objects.filter(name__in=cubes_name).delete()
+
+        dtms = DatasetToMeta.objects.filter(meta__in=metas)
         Dataset.objects.filter(
             id__in=dtms.values_list('dataset', flat=True)).delete()
         dtms.delete()
-
-        cubes_name = ['cube_%s' % x for x in meta_values]
-        Cube.objects.filter(name__in=cubes_name).delete()
 
         meta_keys.delete()
         metas.delete()
