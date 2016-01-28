df89b3fae2e3a68a81a76a0ab295aeef0bc7f406
Andres Rios <rianby64@gmail.com>
2016-01-28 12:02:10 +0300

    Add task for javascript code

--- assets/angular/gulpfile.js
+++ assets/angular/gulpfile.js
@@ -1,7 +1,7 @@
 
 var gulp = require('gulp'),
     watch = require('gulp-watch'),
-    //concat = require('gulp-concat'),
+    concat = require('gulp-concat'),
     minifyJS = require('gulp-uglify'),
     minifyCSS = require('gulp-minify-css'),
     usemin = require('gulp-usemin');
@@ -9,6 +9,7 @@ var gulp = require('gulp'),
 
 var paths = {
   dist: 'dist/',
+  scripts: 'src/js/**/*.js',
   index: 'src/index.html'
 };
 
@@ -30,12 +31,23 @@ gulp.task('usemin', function() {
         }
       )
     )
-    .pipe(gulp.dest('dist/'));
+    .pipe(gulp.dest(paths.dist));
+});
+
+
+gulp.task('main.js', function() {
+  return gulp.src([
+    paths.scripts
+  ])
+  .pipe(minifyJS())
+  .pipe(concat('main.js'))
+  .pipe(gulp.dest(paths.dist));
 });
 
 gulp.task('watch', function() {
   gulp.watch([paths.index], ['usemin']);
+  gulp.watch([paths.scripts], ['main.js']);
 });
 
-gulp.task('default', ['usemin', 'watch']);
+gulp.task('default', ['usemin', 'main.js', 'watch']);
 
