4c2126c447be0dedf99d341b5c9fad7d6718c5ba
Andres Rios <rianby64@gmail.com>
2016-02-01 11:38:08 +0300

    Add functions for removing user

--- assets/angular/src/components/users/view/usersViewCtrl.js
+++ assets/angular/src/components/users/view/usersViewCtrl.js
@@ -6,8 +6,22 @@
 
   function usersViewController($scope, $usersHTTP) {
     $scope.users = [];
+    $scope.currentUser = undefined;
+
     $usersHTTP.read().then(function(response) {
       $scope.users = response;
     });
+
+    $scope.confirmRemove = function confirmRemove() {
+      console.log('confirmRemove item', $scope.currentUser);
+    };
+
+    $scope.cancelRemove = function cancelRemove() {
+      console.log('cancelRemove item', $scope.currentUser);
+    };
+
+    $scope.prepareRemove = function prepareRemove(item) {
+      $scope.currentUser = item;
+    };
   }
 })();
