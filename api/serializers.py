3cbcbfdf60d2e743f2097082ab5b4af75d866c23
ifattakhov <comm.juve@gmail.com>
2016-03-15 10:22:16 +0300

    refs #4747 добавлен id в список отдаваемых полей

--- api/serializers.py
+++ api/serializers.py
@@ -8,13 +8,14 @@ from etl.services.olap.base import send_xml, OlapServerConnectionErrorException
 
 logger = logging.getLogger(__name__)
 
+
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = (
-            'username', 'last_name', 'is_active', 'is_superuser', 'is_staff',
-            'phone', 'skype', 'site', 'city', 'middle_name', 'birth_date',
-            'verify_email_uuid', 'avatar_small', 'avatar')
+            'id', 'username', 'last_name', 'is_active', 'is_superuser',
+            'is_staff', 'phone', 'skype', 'site', 'city', 'middle_name',
+            'birth_date', 'verify_email_uuid', 'avatar_small', 'avatar')
         depth = 1
 
     def create(self, validated_data):
