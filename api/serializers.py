b89ce47e9fa4de5d3211072b6e1454aa5b1c7ec8
ifattakhov <comm.juve@gmail.com>
2016-03-15 10:17:42 +0300

    refs #4747 Расширен список отдаваемых полей для пользователя

--- api/serializers.py
+++ api/serializers.py
@@ -11,8 +11,10 @@ logger = logging.getLogger(__name__)
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('phone', 'skype', 'site', 'city', 'middle_name', 'birth_date',
-                  'verify_email_uuid', 'avatar_small', 'avatar')
+        fields = (
+            'username', 'last_name', 'is_active', 'is_superuser', 'is_staff',
+            'phone', 'skype', 'site', 'city', 'middle_name', 'birth_date',
+            'verify_email_uuid', 'avatar_small', 'avatar')
         depth = 1
 
     def create(self, validated_data):
