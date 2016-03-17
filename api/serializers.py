673e28276876bce6efa653646e5c9c97da3b4cc6
ifattakhov <comm.juve@gmail.com>
2016-03-17 12:21:47 +0300

    refs #4747 @1h передача только названия настройки

--- api/serializers.py
+++ api/serializers.py
@@ -23,11 +23,20 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         depth = 1
 
 
+class SettingsField(serializers.RelatedField):
+    def to_representation(self, value):
+        return value.value
+
+    def to_internal_value(self, data):
+
+        return DatasourceSettings(**{'name': 'cdc_key', 'value': data})
+
+
 class DatasourceSerializer(serializers.ModelSerializer):
     """
     Серилизатор для источника
     """
-    settings = serializers.PrimaryKeyRelatedField(
+    settings = SettingsField(
         many=True, queryset=DatasourceSettings.objects.distinct('name', 'value'))
 
     class Meta:
@@ -42,15 +51,6 @@ class DatasourceSerializer(serializers.ModelSerializer):
             helpers.DataSourceService.delete_datasource(instance)
             helpers.DataSourceService.tree_full_clean(instance)
         return instance
-
-    def create(self, validated_data):
-        datasource_settings = validated_data.pop('settings')
-        datasource = Datasource.objects.create(**validated_data)
-        for ds in datasource_settings:
-            ds.pk = None
-            ds.datasource = datasource
-            ds.save()
-        return datasource
 
 
 class SchemasListSerializer(serializers.ModelSerializer):
