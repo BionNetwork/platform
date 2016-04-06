d23caeb38dfd985724efa3e93485e96300fe19a3
miholeus <me@miholeus.com>
2016-04-06 11:22:28 +0300

    refs #4747 migrations fix

--- core/migrations/0030_auto_20160219_1214.py
+++ core/migrations/0030_auto_20160219_1214.py
@@ -22,4 +22,9 @@ class Migration(migrations.Migration):
             name='avatar_small',
             field=models.ImageField(max_length=500, upload_to=core.helpers.users_avatar_upload_path, null=True, verbose_name='\u0410\u0432\u0430\u0442\u0430\u0440 preview', blank=True),
         ),
+        migrations.AlterField(
+            model_name='dimension',
+            name='type',
+            field=models.CharField(default='SD', max_length=255, verbose_name='\u0442\u0438\u043f \u0438\u0437\u043c\u0435\u0440\u0435\u043d\u0438\u044f', choices=[('SD', 'OTHER'), ('TD', 'TIME')]),
+        ),
     ]
