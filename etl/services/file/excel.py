7d26bbfc21361d388566eaa3d7bef0ca0bc797be
damirGDR <rmcalypse13@mail.ru>
2016-04-18 15:46:54 +0300

    refs #5196 preview files part1

--- etl/services/file/excel.py
+++ etl/services/file/excel.py
@@ -145,21 +145,69 @@ class Excel(File):
                     })
         return intervals
 
+    def generate_join(self, childs):
+        """
+        Принимает структуру дерева и
+        """
+        result = []
+        new_childs = []
+        for child in childs:
+            new_childs += child['childs']
+            result.append({
+                'name': child['val'],
+                'join_type': child['join_type'],
+                'joins': child['joins']
+            })
+        if new_childs:
+            result += self.generate_join(new_childs)
+        return result
+
     def get_rows(self, columns, structure):
 
-        dfs = []
+        # FIXME недоработанная штука, preview работает чуток тока
+
+        left_sheet = structure['val']
+        join_rules = self.generate_join(structure['childs'])
+
+        dfs = {}
         excel_path = self.source.get_file_path()
 
         for sheet_name, col_group in groupby(columns, lambda x: x['table']):
             sheet_df = pandas.read_excel(excel_path, sheetname=sheet_name)
             col_names = [x['col'] for x in col_group]
-            dfs.append(sheet_df[col_names])
-            return sheet_df.fillna('').values.tolist()
+            dfs[sheet_name] = sheet_df[col_names]
 
+        left_df = dfs[left_sheet]
 
+        new_name = "new_name_{0}"
+        merge_columns = []
 
+        for rule in join_rules:
 
+            right_sheet = rule["name"]
+            join_type = rule["join_type"]
+            joins = rule["joins"]
+            new_columns1 = {}
+            new_columns2 = {}
 
+            for i, j in enumerate(joins, start=0):
+                new_name_i = new_name.format(i)
+                new_columns1[j['left']['column']] = new_name_i
+                new_columns2[j['right']['column']] = new_name_i
+                merge_columns.append(new_name_i)
 
+            left_df = left_df.rename(columns=new_columns1)
+            right_df = dfs[right_sheet]
 
+            # right_columns = right_df.columns.tolist()
+            # left_len = len(left_df.columns)
 
+            right_df = right_df.rename(columns=new_columns2)
+
+            # for r_col in new_columns2:
+            #     new_columns2[r_col] = right_columns.index(r_col)
+
+            left_df = pandas.merge(
+                left_df, right_df, how=join_type, on=merge_columns)
+
+        return left_df.fillna('').values.tolist()
